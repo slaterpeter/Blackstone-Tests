0c3661d8d68db8fd51ffd7b67d9cd3d94a843375
Peter Slater <slater.psp@hotmail.com>
2023-06-08 22:23:03 +0000

    Inicial

---
+++ build/lib/blackstone/pipeline/__init__.py
@@ -0,0 +1,4 @@
+from blackstone.pipeline.sentence_segmenter import SentenceSegmenter
+from blackstone.pipeline.concepts import Concepts
+from blackstone.pipeline.abbreviations import AbbreviationDetector
+from blackstone.pipeline.compound_cases import CompoundCases
